85c611a614ae4f9bea7724c7d81c8a69324055c8
Vladyslav Malyshenko <koooilrekt@gmail.com>
2024-03-18 13:24:46 +0200

    Added add to favorite button (bugs are present, will fix soon)

--- src/app/components/workspace/components/workspace/components/playlist/playlist.component.ts
+++ src/app/components/workspace/components/workspace/components/playlist/playlist.component.ts
@@ -3,6 +3,7 @@ import { Observable } from 'rxjs';
 import {
   IPlaylist,
   ISong,
+  PlaylistsService,
 } from '../../../../../../services/playlists-service.service';
 import { SongService } from '../../../../../../services/song.service';
 
@@ -14,12 +15,39 @@ import { SongService } from '../../../../../../services/song.service';
 export class PlaylistComponent implements AfterViewInit {
   @Input() playlist?: Observable<IPlaylist>;
   @Input() path?: string;
+  favoriteStatuses: { [id: string]: boolean } = {};
 
   public currentSong$?: Observable<ISong | undefined>;
 
-  constructor(public songService: SongService) {}
+  constructor(
+    public songService: SongService,
+    public playlistService: PlaylistsService
+  ) {}
+
+  public toggleSongFavorites(e: any, song: ISong): void {
+    if (e.currentTarget) {
+      e.currentTarget.classList.toggle('active');
+      this.playlistService
+        .toggleSongFavorites(song)
+        .subscribe((result) => result);
+    }
+  }
+
+  loadFavorites() {
+    this.playlistService.getCollection().subscribe(() => {
+      this.playlist?.subscribe((playlist: IPlaylist) => {
+        const songs = playlist.songs as ISong[];
+        songs.forEach((song: ISong) => {
+          this.playlistService.checkIfFavorite(song).subscribe((isFavorite) => {
+            this.favoriteStatuses[song.id] = isFavorite;
+          });
+        });
+      });
+    });
+  }
 
   ngAfterViewInit() {
     this.currentSong$ = this.songService.currentSong$;
+    this.loadFavorites();
   }
 }
