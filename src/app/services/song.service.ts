c90a76c8df290a0623ff8527876b557d73e1b8b8
Vladyslav Malyshenko <koooilrekt@gmail.com>
2024-03-18 08:57:35 +0200

    Separated HTMLAudioCustomElement from SongService

--- src/app/services/song.service.ts
+++ src/app/services/song.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import HTMLAudioElementCustom from '../elements/HTMLAudioElementCustom';
 import { LocalStorageService } from './local-storage.service';
 import {
   ICollection,
@@ -11,56 +12,6 @@ import {
 export interface ISongTime {
   currentTime: number;
   duration: number;
-}
-
-class HTMLAudioElementCustom extends HTMLAudioElement {
-  constructor(private songService: SongService) {
-    super();
-  }
-
-  public setRate(value: number) {
-    this.playbackRate = value;
-  }
-
-  public setVolume(value: number) {
-    this.volume = value;
-  }
-
-  public setLoop(value: boolean) {
-    this.loop = value;
-  }
-
-  public setCurrentTime(value: number) {
-    this.currentTime = value;
-  }
-
-  public setSource(value: string) {
-    this.src = value;
-  }
-
-  public skipTo(forward: boolean, step: number) {
-    if (forward) {
-      if (this.currentTime <= this.duration) {
-        this.currentTime += step;
-      } else if (this.currentTime >= this.duration) {
-        this.songService.switchSong(true);
-      }
-    } else {
-      if (this.currentTime > 0) {
-        this.currentTime -= step;
-      } else if (this.currentTime < 0) {
-        this.currentTime = 0;
-      }
-    }
-  }
-
-  public setMuted(value: boolean) {
-    this.muted = value;
-  }
-
-  public getVolume() {
-    return this.volume;
-  }
 }
 
 customElements.define('audio-custom', HTMLAudioElementCustom, {
